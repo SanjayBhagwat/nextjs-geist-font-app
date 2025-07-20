88296dd366fc7a772e0f1e2dd9e20fb5efc33e25
SanjayBhagwat <48258031+SanjayBhagwat@users.noreply.github.com>
2025-07-20 16:39:50 +0530

    Add .next/static/chunks/src_app_login_page_tsx_a993527f._.js 1753009790397

---
+++ .next/static/chunks/src_app_login_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_841b9bda._.js",
+  "static/chunks/src_7a4c577c._.js"
+],
+    source: "dynamic"
+});
